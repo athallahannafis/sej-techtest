28993a0935bac4df63904d95ed29c0ccacc5b7f8
athallahannafis <athallahannafis@gmail.com>
2022-11-16 11:00:41 +0700

    feat: implement pagination

--- src/pages/book.js
+++ src/pages/book.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Table from 'react-bootstrap/Table';
 import axios from "axios";
-import { Image } from "react-bootstrap";
+import { Button, Image, Pagination, Spinner } from "react-bootstrap";
 
 
 
@@ -10,25 +10,66 @@ export default class BookPage extends React.Component {
         super();
         this.state={
             books: [],
+            page: 0,
+            loading: true,
         }
     }
 
     fetchBook = async() => {
-        /// bypass cors policy, request temporary cors bypass in https://cors-anywhere.herokuapp.com/corsdemo
-        /// and then request bypass
-        var corsdemo = "https://cors-anywhere.herokuapp.com/";
-        var url = corsdemo + "https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-books?page=0&categoryId=11";
-        // var url = "https://jsonplaceholder.typicode.com/users";
+        try {
 
-        var response = await axios.get(url);
-        this.setState({
-            books: response.data
-        });
-        console.log(response.data);
+            /**
+             * url to bypass cors policy
+             * https://scrappy-php.herokuapp.com/?url=
+                https://api.allorigins.win/raw?url=
+                https://cors-get-proxy.sirjosh.workers.dev/?url=
+                https://secret-ocean-49799.herokuapp.com/
+                https://cors.eu.org/
+                https://cors-proxy.tk/?url=
 
-        for (let i = 0; i < this.state.books.length; i++) {
-            console.log(this.state.books[i].title);
+
+                https://fast-dawn-89938.herokuapp.com/
+                https://cors-anywhere.herokuapp.com/
+                https://proxy.cors.sh/
+             */
+            this.setState({
+                books: [],
+                loading: true
+            })
+            var corsdemo = "https://fast-dawn-89938.herokuapp.com/";
+            
+            var url = corsdemo + "https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-books?size=7&page="+ this.state.page +"&categoryId=11";
+            // var url = "https://jsonplaceholder.typicode.com/users";
+    
+            var response = await axios.get(url);
+            this.setState({
+                books: response.data,
+                loading: false,
+            });
+            // console.log(response.data);
+        } catch (e) {
+            this.setState({
+                loading: false,
+            });
         }
+        
+    }
+
+    pagination = (flag) =>{
+        if (flag == "Next") {
+            this.setState({
+                page: this.state.page+1,
+            });
+            console.log(flag);
+        } else {
+            if (this.state.page > 0) {
+                this.setState({
+                    page: this.state.page-1,
+                })
+            }
+            console.log(flag);
+        }
+        this.fetchBook();
     }
 
     componentDidMount = () => {
@@ -38,13 +79,16 @@ export default class BookPage extends React.Component {
     render() {
         return (
             <div className="flex min-h-screen flex-col justify-center items-center  py-10 sm:py-12">
-               <Table responsive striped>
+                {
+                this.state.loading == false ?
+               <Table responsive hover striped>
                 <thead>
                     <tr>
+                        <th>Bookmark</th>
                         <th>Cover</th>
                         <th>Book title</th>
                         <th>Description</th>
-                        {/* <th>Authors</th> */}
+                        <th>Authors</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -52,16 +96,35 @@ export default class BookPage extends React.Component {
                         return  (
                             <tr key={index}>
                                 <td>
-                                    <img src={value.cover_url}/>
+                                    
+                                </td>
+                                <td>
+                                    <img width={200} src={value.cover_url}/>
                                 </td>
                                 <td>{value.title}</td>
                                 <td>{value.description}</td>
-
+                                <td>{value.authors}</td>
                             </tr>
                         )
                     })}
                 </tbody>
-                </Table>
+                </Table> : 
+                <Spinner animation="border" />
+                }
+                <div className="my-10"/>
+                <Pagination>
+                    <Pagination.Prev onClick={() => {this.pagination("Prev")}} />
+                    <Pagination.Item>{this.state.page}</Pagination.Item>
+                    <Pagination.Next onClick={() => {this.pagination("Next")}} />
+                </Pagination>
+                <div className="flex flex-row justify-center content-center">
+                    {/* <Button onClick={() => {this.pagination("Prev")}} variant="outline-primary">Prev</Button>
+                    <div className="w-2"/>
+                    <p>{this.state.page}</p>
+                    <div className="w-2"/>
+                <Button onClick={() => {this.pagination("Next")}} variant="outline-primary">Next</Button> */}
+                </div>
+
             </div>
         );
     }
